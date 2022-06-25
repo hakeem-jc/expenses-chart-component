2516bb73d610deda3eb17bde308e66809d394aa0
VEN-ClarkeHaJ <clarkehaj@jncb.com>
2022-06-25 12:08:00 -0500

    Write markup for Balance component

---
+++ src/components/Balance/index.js
@@ -0,0 +1,15 @@
+import { Logo } from 'images/logo.svg';
+
+export const Balance = () => {
+    return (
+        <header className="balance">
+            <div className="balance__content">
+                <h3 className="balance__title">My balance</h3>
+                <h1 className="balance__value">$921.48</h1>
+            </div>
+            <div className="balance__logo_container">
+                <img src={Logo} alt="App logo"/>
+            </div>
+        </header>
+    )
+}
