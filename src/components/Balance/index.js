5ddf2a5ce6ddc2dc935a8464848747999e3ab2e6
hakeem-jc <hclarke.dev@gmail.com>
2022-07-19 08:52:43 -0500

    Style balance section

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -6,7 +6,7 @@ export const Balance = () => {
         <header className="balance">
             <div className="balance__content">
                 <h3 className="balance__title">My balance</h3>
-                <h1 className="balance__value">$921.48</h1>
+                <h2 className="balance__value">$921.48</h2>
             </div>
             <div className="balance__logo_container">
                 <img src={Logo} alt="App logo"/>
