4264039d33ddff002d98b0e1ef7d3d5f2c29cb91
hakeem-jc <hclarke.dev@gmail.com>
2022-12-20 22:24:16 -0500

    Update text from heading to p tags

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -17,17 +17,17 @@ export const Chart = () => {
 
     return (
         <section className="chart">
-            <h1 className="chart__title">Spending - Last 7 days</h1>
+            <h4 className="chart__title">Spending - Last 7 days</h4>
             <div className="chart__bars">{bars}</div>
             <div className="chart__summary">
                 <h5 className="chart__text">Total this month</h5>
                 <div className="chart__summary-flex">
                     <div className="chart__summary-flex-left">
-                        <h1 className="chart__number">$478.33</h1>
+                        <p className="chart__number">$478.33</p>
                     </div>
                     <div className="chart__summary-flex-right">
-                        <h3 className="chart__number-change">+2.4%</h3>
-                        <h5 className="chart__text">from last month</h5>
+                        <p className="chart__number-change">+2.4%</p>
+                        <p className="chart__text">from last month</p>
                     </div>
                 </div>
             </div>
